7afdef6fa723a4d2565b8badb74def63514c79cd
Sanjay <82385800+sanjay7708@users.noreply.github.com>
2023-01-10 12:43:38 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,20 @@
+def Binary(array,start,end,n):
+	mid=(start+end)//2
+	if array[mid]==n:
+		print(mid)
+	else:
+		while start<end:
+			if array[mid]>n:
+				return Binary(array,start,mid-1,n)
+			elif array[mid]<n:
+				return Binary(array,mid+1,end,n)
+		return 
+if __name__=="__main__":
+	array=[1,2,3,4,5,6,7,8,9,10]
+	start=array[0]
+	end=array[-1]
+	print(array)
+	n=int(input("Which Element Do Yo Want Search?:"))
+	result=Binary(array,start,end,n)
+	print("the element",n,"indext is:",result)
+	
